eb1842a852f384650684dc83446790277e8f33c8
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-09-02 12:28:12 +0100

    Add feature section

--- app/(sass1)/page.tsx
+++ app/(sass1)/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/sass1/Banner";
+import Features from "@/components/sass1/Features";
 import Hero from "@/components/sass1/Hero";
 import LogoTicker from "@/components/sass1/LogoTicker";
 import Navbar from "@/components/sass1/Navbar";
@@ -10,6 +11,7 @@ const page = () => {
       <Navbar />
       <Hero />
       <LogoTicker />
+      <Features />
     </div>
   );
 };
--- components/sass1/LogoTicker.tsx
+++ components/sass1/LogoTicker.tsx
@@ -18,7 +18,7 @@ const LogoTicker = () => {
           Trusted by the world's most innovative teams
         </h2>
         <div className="overflow-hidden relative mt-9 before:content-[''] before:absolute before:h-full after:h-full after:absolute before:w-5 after:w-5 after:right-0 before:left-0 after:top-0 before:top-0 after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))] before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))]">
-          <div className="flex gap-4 justify-center items-center">
+          <div className="flex gap-14 justify-center items-center">
             {partnersLogo.map((partnerLogo) => (
               <Image
                 src={partnerLogo}
