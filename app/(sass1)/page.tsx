1c5f5ef723fff57181746234338677812a259580
abdulmaleekadams <harbdulmaleek@gmail.com>
2024-09-02 04:42:22 +0100

    Add hero section

--- app/(sass1)/page.tsx
+++ app/(sass1)/page.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/components/sass1/Banner";
+import Hero from "@/components/sass1/Hero";
 import Navbar from "@/components/sass1/Navbar";
 
 const page = () => {
@@ -6,6 +7,7 @@ const page = () => {
     <div className="flex flex-col items-center min-h-screen  *:w-full">
       <Banner />
       <Navbar />
+      <Hero />
     </div>
   );
 };
